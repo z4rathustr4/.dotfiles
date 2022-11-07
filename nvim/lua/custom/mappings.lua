5a91f450281c1236974848680f04b481ea6e13cd
z4rathustr4 <ringzer0dev@gmail.com>
2022-11-07 10:09:48 +0000

    Formatting

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.general = {
   v = {
-
+    -- Visual mode mappings (vnoremap)
     ["<leader>y"] = {"\"+y", "borrowed from ThePrimagean who stole it from someone else kekw"},
     ["<A-k>"] = {":m '<-2<CR>gv=gv", "move block of code up one line (visual mode)"},
     ["<A-j>"] = {":m '>+1<CR>gv=gv", "move block of code down one line (normal mode)"},
@@ -19,8 +19,8 @@ M.general = {
     ["kk"] = {"<ESC>", "exit insert mode", opts = { nowait = true }},
   },
   n = {
+    -- Normal mode mappings (nnoremap)
     ["<leader>y"] = {"\"+y", "borrowed from ThePrimagean who stole it to someone else kekw"},
-
     ["<leader>s"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "global substitution of a pattern" },
     ["<A-j>"] = {":m '>+1<CR>gv=gv", "move block of code down one line (normal mode)"},
     ["<A-k>"] = {":m '<-2<CR>gv=gv", "move block of code up one line (normal mode)"},
